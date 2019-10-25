f8ce2b1b3477c407388f813d01770e03db91a957
Raphaël HERVEUX <raphael.herveux@seetymood.com>
2019-10-25 11:31:13 +0200

    Trying without Foreground listener

--- android/src/main/java/com/jamesisaac/rnbackgroundtask/RNJob.java
+++ android/src/main/java/com/jamesisaac/rnbackgroundtask/RNJob.java
@@ -1,6 +1,5 @@
 package com.jamesisaac.rnbackgroundtask;
 
-import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -11,12 +10,9 @@ import androidx.work.WorkerParameters;
 
 import com.facebook.react.HeadlessJsTaskService;
 
-
-import java.util.List;
-
 /**
  * The single task which this library is able to schedule.
- *
+ * <p>
  * The sole purpose here is to kick off the HeadlessTaskService, and pass along the config params
  * which were used when creating the job.
  */
@@ -31,36 +27,11 @@ public class RNJob extends Worker {
     @Override
     public Result doWork() {
         Context context = getApplicationContext();
-        if (isAppOnForeground(context)) {
-            Log.d(TAG, "Job is running");
-            Intent serviceIntent = new Intent(context, HeadlessTaskService.class);
-            context.startService(serviceIntent);
-            HeadlessJsTaskService.acquireWakeLockNow(context);
-        }
-        Log.d(TAG, "Job running aborted as app is in foreground");
+        Log.d(TAG, "Job is running");
+        Intent serviceIntent = new Intent(context, HeadlessTaskService.class);
+        context.startService(serviceIntent);
+        HeadlessJsTaskService.acquireWakeLockNow(context);
         return Result.success();
-    }
-
-    private boolean isAppOnForeground(Context context) {
-        /**
-         We need to check if app is in foreground otherwise the app will crash.
-         http://stackoverflow.com/questions/8489993/check-android-application-is-in-foreground-or-not
-         **/
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> appProcesses =
-                activityManager.getRunningAppProcesses();
-        if (appProcesses == null) {
-            return false;
-        }
-        final String packageName = context.getPackageName();
-        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
-            if (appProcess.importance ==
-                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
-                    appProcess.processName.equals(packageName)) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
