e246cf102f414f346b398c8c47e20f849d39dbb8
Raphaël HERVEUX <raphael.herveux@seetymood.com>
2019-10-25 10:47:50 +0200

    Remove JobManager reference

--- android/src/main/java/com/jamesisaac/rnbackgroundtask/BackgroundTaskPackage.java
+++ android/src/main/java/com/jamesisaac/rnbackgroundtask/BackgroundTaskPackage.java
@@ -1,25 +1,16 @@
 package com.jamesisaac.rnbackgroundtask;
 
-import android.app.Application;
-import com.evernote.android.job.JobManager;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class BackgroundTaskPackage implements ReactPackage {
-    /**
-     * android-job needs to be set up with the Application context, so provide a convenience method
-     * which users can add to the Application.onCreate.
-     */
-    public static void useContext(Application context) {
-        JobManager.create(context).addJobCreator(new RNJobCreator());
-    }
-
     @Override
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
